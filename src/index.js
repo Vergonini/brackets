c8d5bee12bc82d46a078f61f5811cd3da9e49aa0
Pavel Khareuka <paul.horevko@gmail.com>
2019-03-20 22:14:00 +0300

    feat: add check for even number of characters

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
+  if (str.length % 2 != 0) return false;
   let array = str.split('');
-  let config = bracketsConfig;
   let stack = [];
 
   for (let k = 0; k < array.length; k++) {
-    for (let i = 0; i < config.length; i++) {
-      if (array[k] == config[i][0]) {
-        if(stack[stack.length-1] == array[k] && config[i][0] == config[i][1]) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (array[k] == bracketsConfig[i][0]) {
+        if(stack[stack.length-1] == array[k] && bracketsConfig[i][0] == bracketsConfig[i][1]) {
           stack.pop();
           break;
         }
-        stack.push(config[i][0]);
-        break;
-      } else if(array[k] == config[i][1]) {
-        if (stack[stack.length-1] == config[i][0]) {
-          stack.pop();
-          break;
-        }
+        stack.push(bracketsConfig[i][0]);
+      } else if (array[k] == bracketsConfig[i][1] && stack[stack.length-1] == bracketsConfig[i][0]) {
+        stack.pop();
       }
     }
   }
-  
+
   if (stack.length == 0) return true;
   return false;
 }
